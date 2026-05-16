fd90693a07b88fc152c45bd2ed9a072b25c63bb4
Hashidaru978 <ashitanoyab@gmail.com>
2026-05-16 13:53:27 +0000

    add exo2

---
+++ exo2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$age = 25;
+$sexe = "F";
+if($sexe == "F" && $age >= 21 && $age <= 40){
+    echo "Bienvenue madame";
+}
+    ?>
+</body>
+</html>
