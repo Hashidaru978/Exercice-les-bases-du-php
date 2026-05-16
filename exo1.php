c50301dee1cbe9c741dbb68c93ff9339ad4b474f
Hashidaru978 <ashitanoyab@gmail.com>
2026-05-16 13:53:27 +0000

    add exo1

---
+++ exo1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$nombre = 15;
+if($nombre % 3 == 0 && $nombre % 5 == 0){
+    echo "Le nombre est multiple de 3 et de 5";
+}
+    ?>
+</body>
+</html>3
